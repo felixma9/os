cd42d97861d31eae03f10727e6237bf76604c57a
Felix Ma <112669786+felixma9@users.noreply.github.com>
2026-06-06 19:39:15 -0700

    C implementation of FAT12

--- Makefile
+++ Makefile
@@ -2,8 +2,11 @@ ASM=nasm
 
 SRC_DIR=src
 BUILD_DIR=build
+TOOLS_DIR=tools
 
-.PHONY: all floppy_image kernel bootloader clean always run
+.PHONY: all floppy_image kernel bootloader clean always run tools_fat
+
+all: floppy_image tools_fat
 
 # Floppy image
 floppy_image: $(BUILD_DIR)/main_floppy.img
@@ -13,6 +16,7 @@ $(BUILD_DIR)/main_floppy.img: bootloader kernel
 	mkfs.fat -F 12 -n "NBOS" $(BUILD_DIR)/main_floppy.img
 	dd if=$(BUILD_DIR)/bootloader.bin of=$(BUILD_DIR)/main_floppy.img conv=notrunc
 	mcopy -i $(BUILD_DIR)/main_floppy.img $(BUILD_DIR)/kernel.bin "::kernel.bin"
+	mcopy -i $(BUILD_DIR)/main_floppy.img test.txt "::test.txt"
 
 # Bootloader
 bootloader: $(BUILD_DIR)/bootloader.bin
@@ -37,3 +41,10 @@ clean:
 # Run macro
 run:
 	qemu-system-i386 -fda build/main_floppy.img
+
+# Build tools
+tools_fat: $(BUILD_DIR)/tools/fat
+
+$(BUILD_DIR)/tools/fat: always $(TOOLS_DIR)/fat/fat.c
+	mkdir -p $(BUILD_DIR)/tools
+	gcc -g -o $(BUILD_DIR)/tools/fat $(TOOLS_DIR)/fat/fat.c
---
+++ tools/fat/fat.c
@@ -0,0 +1,203 @@
+// Implementing fat in C to learn it first
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef uint8_t bool;
+#define true 1
+#define false 0
+
+typedef struct {
+    uint8_t BootJumpInstruction[3];
+    uint8_t OemIdentifier[8];
+    uint16_t BytesPerSector;
+    uint8_t  SectorsPerCluster;
+    uint16_t ReservedSectors;
+    uint8_t FatCount;
+    uint16_t DirEntryCount;
+    uint16_t TotalSectors;
+    uint8_t MediaDescriptorType;
+    uint16_t SectorsPerFat;
+    uint16_t SectorsPerTrack;
+    uint16_t Heads;
+    uint32_t HiddenSectors;
+    uint32_t LargeSectorCount;
+
+    // extended boot record
+    uint8_t DriveNumber;
+    uint8_t _Reserved;
+    uint8_t Signature;
+    uint32_t VolumeId;          // serial number, value doesn't matter
+    uint8_t VolumeLabel[11];    // 11 bytes, padded with spaces
+    uint8_t SystemId[8];
+
+    // don't care about the remaining code
+} __attribute__((packed)) BootSector; // in gcc, this attribute macro is used to ensure
+                                      // compiler doesn't pad our struct
+
+// One entry in the directory, according to FAT12 documentation
+typedef struct {
+    uint8_t Name[11];
+    uint8_t Attributes;
+    uint8_t _Reserved;
+    uint8_t CreatedTimeTenths;
+    uint16_t CreatedTime;
+    uint16_t CreatedDate;
+    uint16_t AccessedDate;
+    uint16_t FirstClusterHigh;
+    uint16_t ModifiedTime;
+    uint16_t ModifiedDate;
+    uint16_t FirstClusterLow;
+    uint32_t Size;      // size of the file in bytes
+} __attribute__((packed)) DirectoryEntry;
+
+// global boot sector
+BootSector g_BootSector;
+uint8_t* g_Fat = NULL;
+DirectoryEntry* g_RootDirectory = NULL;
+uint32_t g_RootDirectoryEnd;        // Save where root dir ends and data begins
+
+// Load g_BootSector with the data from the floppy_image (headers loaded in boot.asm)
+int readBootSector(FILE* disk) {
+    return fread(&g_BootSector, sizeof(g_BootSector), 1, disk);
+}
+
+bool readSectors(FILE* disk, uint32_t lba, uint32_t count, void* bufferOut) {
+    bool ok = true;
+    ok = ok && (fseek(disk, lba * g_BootSector.BytesPerSector, SEEK_SET) == 0);
+    ok = ok && (fread(bufferOut, g_BootSector.BytesPerSector, count, disk) == count);
+    return ok;
+}
+
+bool readFat(FILE* disk) {
+    g_Fat = (uint8_t*) malloc(g_BootSector.SectorsPerFat * g_BootSector.BytesPerSector);
+    return readSectors(disk, g_BootSector.ReservedSectors, g_BootSector.SectorsPerFat, g_Fat);
+}
+
+bool readRootDirectory(FILE* disk) {
+    // Calculate start of data section
+    uint32_t lba = g_BootSector.ReservedSectors + g_BootSector.SectorsPerFat * g_BootSector.FatCount;
+    uint32_t size = sizeof(DirectoryEntry) * g_BootSector.DirEntryCount;
+    uint32_t sectors = (size / g_BootSector.BytesPerSector);
+    if (size % g_BootSector.BytesPerSector > 0) {
+        // Round up number of sectors needed
+        sectors++;
+    }
+
+    g_RootDirectoryEnd = lba + sectors;
+    g_RootDirectory = (DirectoryEntry*) malloc(sectors * g_BootSector.BytesPerSector);
+    return readSectors(disk, lba, sectors, g_RootDirectory);
+}
+
+DirectoryEntry* findFile(const char* name) {
+    for (uint32_t i = 0; i < g_BootSector.DirEntryCount; ++i) {
+        if (memcmp(name, g_RootDirectory[i].Name, 11) == 0) {
+            return &g_RootDirectory[i];
+        }
+    }
+
+    return NULL;
+}
+
+bool readFile(DirectoryEntry* fileEntry, FILE* disk, uint8_t* outputBuffer) {
+    bool ok = true;
+    uint16_t currentCluster = fileEntry->FirstClusterLow;
+
+    // Read each cluster
+    do {
+        // Convert from cluster to sector
+        uint32_t lba = g_RootDirectoryEnd + (currentCluster - 2) * g_BootSector.SectorsPerCluster;
+        ok = ok && readSectors(disk, lba, g_BootSector.SectorsPerCluster, outputBuffer);
+        outputBuffer += g_BootSector.SectorsPerCluster * g_BootSector.BytesPerSector;
+
+        // Determine next cluster (look up in FAT table)
+        // Note that entries are 12 bits wide (1.5 bytes), which is a little annoying (Bytes can store half of one entry and half another)
+        //      FF F0 00 -> this is actually 2 entries, split across 3 bytes
+        // Multiply current cluster by 3 then divide by 2 to get offset into table
+        uint32_t fatIndex = currentCluster * 3 / 2;
+
+        // Case 1: currentCluster is even, take lower bits
+        if (currentCluster % 2 == 0) {
+            currentCluster = (*(uint16_t*)(g_Fat + fatIndex)) & 0x0FFF;
+        }
+        // Case 2: currentCluster is odd, take upper 12 bits
+        else {
+            currentCluster = (*(uint16_t*)(g_Fat + fatIndex)) >> 4;
+        }
+
+    } while (ok && currentCluster < 0xFF8); // Once cluster reaches 0xFF8, the chain is complete (reached end of file)
+
+    return ok;
+}
+
+int main(int argc, char** argv) {
+
+    // Check that CLI argument number is correct
+    if (argc < 3) {
+        printf("Syntax: %s <disk_image> <file_name>, where file_name is all caps, and 11 bytes (pad with space)\n", argv[0]);
+        return -1;
+    }
+
+    FILE* disk = fopen(argv[1], "rb");
+    if (!disk) {
+        fprintf(stderr, "Cannot open disk image %s!", argv[1]);
+        return -1;
+    }
+
+    if (!readBootSector(disk)) {
+        fprintf(stderr, "Could not read boot sector!\n");
+        return -2;
+    }
+
+    if (!readFat(disk)) {
+        fprintf(stderr, "Could not read FAT!\n");
+        free(g_Fat);
+        return -3;
+    }
+
+    if (!readRootDirectory(disk)) {
+        fprintf(stderr, "Could not read FAT!\n");
+        free(g_Fat);
+        free(g_RootDirectory);
+        return -4;
+    }
+
+    DirectoryEntry* fileEntry = findFile(argv[2]);
+    if (!fileEntry) {
+        fprintf(stderr, "Could not find file %s!\n", argv[2]);
+        free(g_Fat);
+        free(g_RootDirectory);
+        return -5;
+    }
+
+    // Allocate a buffer into which we read the file from disk
+    // Files take up a minimum of 1 cluster, so allocate clusters as needed
+    uint32_t bytesPerCluster = g_BootSector.BytesPerSector * g_BootSector.SectorsPerCluster;
+    uint32_t clusters = (fileEntry->Size + bytesPerCluster - 1) / bytesPerCluster;
+    uint32_t bufferSize = clusters * bytesPerCluster;
+
+    uint8_t* buffer = (uint8_t*) malloc(bufferSize);
+    if (!readFile(fileEntry, disk, buffer)) {
+        fprintf(stderr, "Could not read file %s!\n", argv[2]);
+        free(g_Fat);
+        free(g_RootDirectory);
+        free(buffer);
+        return -6;
+    }
+
+    // Now print the file we read
+    for (size_t i = 0; i < fileEntry->Size; ++i) {
+        if (isprint(buffer[i])) {
+            fputc(buffer[i], stdout);
+        } else {
+            printf("<%02x>", buffer[i]);
+        }
+    }
+    printf("\n");
+
+    free(g_Fat);
+    free(g_RootDirectory);
+    return 0;
+}
